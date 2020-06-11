7f523c1018b0915a128b4a3db49c8ae389c43861
ArtemErmulin <artem.ermulin@ya.ru>
2020-06-11 22:45:35 +0300

    Add parsing timepad

--- escraper/parsers.py
+++ escraper/parsers.py
@@ -23,19 +23,53 @@ class Timepad(BaseParser):
     """
     name = "timepad"
     url = "www.timepad.ru"
+    events_api = "https://api.timepad.ru/v1/events/{event_id}"
 
-    def parse(self, url):
-        pass
+    def __init__(self):
+        # TODO add as enviroment variable
+        path = os.path.join(os.path.dirname(__file__), "misk/timepad_token")
+        with open(path) as f:
+            self._token = f.readline()
+
+        self.headers = dict(
+            Authorization=f"Bearer {self._token}",
+        )
+
+    def parse(self, event_id):
+        url = self.events_api.format(event_id=event_id)
+        res = requests.get(url=url, headers=self.headers)
+
+        if not res.ok:
+            raise ValueError("Bad request, reason: {}".format(res.reson))
+
+        r = res.json()
+
+        data = dict(
+            starts_at=r["starts_at"],
+            ends_at=r["ends_at"],
+            name=r["name"],
+            description_short=r["description_short"],
+            url=r["url"],
+            poster_image=r["poster_image"]["default_url"],
+            city=r["location"]["city"],
+            org_name=r["organization"]["name"],
+            categories=r["categories"],
+            tickets_limit=r["tickets_limit"],
+            ticket_types=r["ticket_types"],
+            age_limit=r["age_limit"],
+            access_status=r["access_status"],
+            is_registration_open=r["registration_data"]["is_registration_open"],
+        )
+        return data
 
     def get_events(self):
-        return "Some events..."
+        # testing
+        event_id = 1330000
+        return self.parse(event_id)
 
 
 class Parser:
-    def __init__(self):
-        pass
-
-    def get_events(self, *args, source=None, **kwargs):
+    def get_events(self, source=None, *args, **kwargs):
         if source is None:
             parser = random.choice(list(_PARSERS.values()))
         elif source not in _PARSERS:
