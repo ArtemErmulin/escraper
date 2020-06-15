e4654ffdd48da2a45408564691bbb521075c0d7b
ArtemErmulin <artem.ermulin@ya.ru>
2020-06-15 10:06:20 +0300

    Update version to 0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as fp:
 
 setuptools.setup(
     name="event-scraper",
-    version="0.0.1",
+    version="0.0.2",
     packages=setuptools.find_packages(),
     install_requires=install_requires,
     include_package_data=True,
