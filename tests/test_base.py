11311441358f843e269da5445f83e9b84c818d95
ArtemErmulin <artem.ermulin@ya.ru>
2020-09-08 15:30:15 +0300

    Add more test for timepad

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,14 +1,30 @@
 from datetime import datetime as dt
 
+import pytest
+
 from escraper.parsers import Timepad, Radario
 
 
-def test_timepad():
-    # get timepad token from environ
-    t = Timepad()
-    events = t.get_events()
 
-    assert events
+@pytest.fixture
+def timepad():
+    return Timepad()
+
+
+def test_timepad_get_events(timepad):
+    assert timepad.get_events()
+
+
+def test_timepad_event_catogories(timepad):
+    assert timepad.event_categories
+
+
+def test_timepad_event_statuses(timepad):
+    assert timepad.event_statuses
+
+
+def test_timepad_ticket_statuses(timepad):
+    assert timepad.tickets_statuses
 
 
 def test_radario():
