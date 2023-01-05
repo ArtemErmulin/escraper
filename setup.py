d0fd0463b9f352502666d45494d10b0f6be7f945
magusch <magusch@gmail.com>
2023-01-05 17:47:56 +0300

    New version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as fp:
 
 setuptools.setup(
     name="escraper",
-    version="1.1.2",
+    version="1.1.3",
     packages=setuptools.find_packages(),
     install_requires=install_requires,
     include_package_data=True,
