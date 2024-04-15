dddd0b112522aa8affc52c87aae0ef6b0df6dc17
magusch <magusch@gmail.com>
2024-04-15 17:32:30 +0700

    version 1.1.8.0: add MTS live parsing

--- escraper/parsers/__init__.py
+++ escraper/parsers/__init__.py
@@ -4,11 +4,13 @@ from .radario import Radario
 from .ticketscloud import Ticketscloud
 from .vk import VK
 from .qtickets import QTickets
+from .mts import MTS
 
 
 all_parsers = dict(
     timepad=Timepad,
     radario=Radario,
     ticketscloud=Ticketscloud,
-    vk=VK
+    vk=VK,
+    mts=MTS,
 )
